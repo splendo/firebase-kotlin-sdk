69aede9bfb109d75bc9599cc59f66ea2ede311be
Carmelo Gallo <work@carmelogallo.com>
2021-07-22 10:11:44 +0200

    Part 1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    kotlin("multiplatform") version "1.4.31" apply false
-    kotlin("plugin.serialization") version "1.4.31" apply false
+    kotlin("multiplatform") version "1.5.30-M1" apply false
+    kotlin("plugin.serialization") version "1.5.30-M1" apply false
     id("base")
 }
 
